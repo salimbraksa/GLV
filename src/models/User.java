3d1c1a16553af7ff70092c8f8c2d8da761811676
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-04-27 23:43:04 +0100

    created user model

--- src/models/User.java
+++ src/models/User.java
@@ -4,4 +4,10 @@ package models;
  * Created by Salim on 4/27/16.
  */
 public class User {
+    private String firstName;
+
+    public User(String name){
+        this.firstName = name;
+    }
+    
 }
