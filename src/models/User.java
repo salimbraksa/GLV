e97368e7365786b93f4ec64c65ed04614e4a2a5f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-04-28 13:18:35 +0100

    finished the user model

--- src/models/User.java
+++ src/models/User.java
@@ -4,10 +4,25 @@ package models;
  * Created by Salim on 4/27/16.
  */
 public class User {
-    private String firstName;
 
-    public User(String name){
-        this.firstName = name;
+    // User attributes
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String sexe;
+    private String email;
+    private String phone;
+
+    // User constructor
+
+    public User(int id,String first_name, String last_name, String sexe, String email, String phone){
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.sexe = sexe;
+        this.email = email;
+        this.phone = phone;
     }
-    
+
 }
