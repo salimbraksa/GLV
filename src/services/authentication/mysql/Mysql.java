f7c56128ae6a608e7b78c2c19b918191f267c24a
salimbraksa <salim.braksa@gmail.com>
2016-05-02 22:13:21 +0100

    Ability to connect to database

---
+++ src/services/authentication/mysql/Mysql.java
@@ -0,0 +1,63 @@
+package services.authentication.mysql;
+
+import java.sql.*;
+
+/**
+ * Created by Salim on 4/26/16.
+ */
+public class Mysql {
+
+    private static Mysql sharedInstance = new Mysql();
+    public static Mysql sharedInstance() {
+        return sharedInstance;
+    }
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private Mysql() {
+
+        try {
+
+            // register the driver
+            String sDriverName = "org.sqlite.JDBC";
+            Class.forName(sDriverName);
+
+            // Connect
+            connection = DriverManager.getConnection("jdbc:sqlite:src/resources/database/production.db");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+    // Queries Methods
+
+    public ResultSet executeQuery(String query) {
+        Statement stmt = null;
+        ResultSet result = null;
+        try {
+            stmt = connection.createStatement();
+            result = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void executeUpdate(String update) {
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            stmt.executeUpdate(update);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
