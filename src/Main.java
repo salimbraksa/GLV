2855fb1fc338740ac0e7216b560496d536bb28a1
salimbraksa <salim.braksa@gmail.com>
2016-05-03 22:04:29 +0100

    Added password field

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,10 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
         primaryStage.setResizable(false);
-
         Mysql mysql = Mysql.sharedInstance();
+
+        
+
 
     }
 
