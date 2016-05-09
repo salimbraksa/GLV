a30ff6bf8ca6250aa9673e13ff157bc6e74c112a
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-05-09 21:11:20 +0100

    completing UserStore

--- src/services/stores/UserStore.java
+++ src/services/stores/UserStore.java
@@ -1,23 +1,27 @@
 package services.stores;
 
 import models.User;
+import services.mysql.Mysql;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 /**
  * Created by Salim on 5/4/16.
  */
-public class UserStore implements StoreType<User> {
+public class UserStore implements StoreType<User>  {
 
     // Singleton Implementation
 
     static private UserStore singleton = new UserStore();
 
-    static public UserStore sharedInstance() {
-        return singleton;
-    }
+    static public UserStore sharedInstance() { return singleton; }
 
     private UserStore() { }
+
+    Mysql mysql = Mysql.sharedInstance();
 
     // CRUD Methods
 
@@ -37,6 +41,24 @@ public class UserStore implements StoreType<User> {
 
     @Override
     public User find(int id) {
+        ResultSet result = mysql.executeQuery("SELECT * FROM user WHERE id="+id);
+
+        //Retrieve data by column name
+        try {
+            int userId = result.getInt("id");
+            String userFirstName = result.getString("first_name");
+            String userLastName = result.getString("last_name");
+            String userSexe = result.getString("sexe");
+            String userEmail = result.getString("email");
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ///User user = new User();
         return null;
     }
 
