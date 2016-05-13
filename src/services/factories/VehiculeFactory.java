c512d7775ebcbf5179aad42c012b61d4a4d8b2bd
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-05-13 20:20:39 +0100

    finished VehiculeFactory

--- src/services/factories/VehiculeFactory.java
+++ src/services/factories/VehiculeFactory.java
@@ -4,6 +4,7 @@ import models.Vehicule;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by chaymaebz on 12/05/16.
@@ -23,8 +24,30 @@ public class VehiculeFactory {
      // Methods
 
     public Vehicule getTransformedValue() throws SQLException {
-        
-        return null;
+
+        if (result.next()){
+            int id = result.getInt("id");
+            String type = result.getString("type");
+            Double price = result.getDouble("price");
+            Vehicule.State state = Vehicule.State.valueOf(result.getString("state"));
+
+            return new Vehicule(id,type,price,state);
+        }
+        else {
+            return null;
+        }
     }
+
+     public ArrayList<Vehicule> getTransformedValues() throws SQLException {
+        ArrayList<Vehicule> vehicules = new ArrayList<>();
+
+        while (result.next()){
+            Vehicule vehicule = getTransformedValue();
+            vehicules.add(vehicule);
+        }
+
+        return vehicules;
+    }
+
 
 }
