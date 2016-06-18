4554fc469d0f6ecdac29ccb13d36890b555f10db
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-06-18 18:33:59 +0000

    Validatable interface

--- src/services/validation/Validatable.java
+++ src/services/validation/Validatable.java
@@ -1,10 +1,14 @@
 package services.validation;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Created by Salim on 5/9/16.
  */
 public interface Validatable {
 
+    ArrayList<Error> validate(Map<String, Object> additionalInfos);
 
 
 }
